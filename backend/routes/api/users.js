1e48fa2ab5134b5464589b3bb1feee1453cedd17
cooksey <andrewcooksey42@gmail.com>
2022-02-04 12:31:24 -0600

    login and logout routes

---
+++ backend/routes/api/users.js
@@ -0,0 +1,18 @@
+// const router = require('express').Router();
+// const sessionRouter = require('.session.js');
+// const usersRouter = require('./users.js');
+
+// router.use('/session', sessionRouter);
+
+// router.use('/users', usersRouter);
+
+// router.post('/test', (req, res) => {
+//     res.json({ requestBody: req.body });
+// });
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
